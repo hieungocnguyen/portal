44f7ab67759348b1d3310ac59f0f5ba15d9d11d5
hieungocnguyen <ngnohieu@gmail.com>
2026-03-07 14:25:11 +0700

    Add favicon and Apple touch icon links to RootLayout for improved branding and user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <script defer src="https://cloud.umami.is/script.js" data-website-id="ea65a8d2-5ebe-4c05-a9f1-3bf245b19c53"></script>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className={`${jetbrainsMono.variable} antialiased`}
